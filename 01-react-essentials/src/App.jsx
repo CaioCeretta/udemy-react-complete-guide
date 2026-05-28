1656d8f8deb2a2bf46016db486ad7c3f01885493
Caio <caio.soares@vcimentos.com>
2026-05-27 23:33:59 -0300

    doc: up to state

--- 01-react-essentials/src/App.jsx
+++ 01-react-essentials/src/App.jsx
@@ -2,8 +2,17 @@ import { CORE_CONCEPTS } from "./data";
 import { CoreConcept } from "./components/CoreConcepts";
 import { Header } from "./components/Header/Header";
 import TabButton from "./components/TabButton";
+import { useState } from "react";
 
 function App() {
+
+  const [tabContent, setTabContent] = useState('Please, select a button')
+
+  function handleSelect(selectedButton) {
+    // selected button => 'components' | 'jsx' | 'props' | 'state'
+    setTabContent(selectedButton)
+  }
+
   return (
     <div>
       <header>
@@ -26,11 +35,12 @@ function App() {
         <section id='examples'>
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
-          </menu>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu> 
+          {tabContent}
         </section>
       </main>
     </div>
